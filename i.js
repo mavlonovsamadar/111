1de3cc1504a8ba1ade6ba3505718dcb6d2913906
samandar92 <mavlonovsamandar1992@gmail.com>
2022-11-23 20:28:42 +0500

    fix

---
+++ i.js
@@ -0,0 +1,68 @@
+
+//////////////////// Async await//////////////////////////
+
+// const url = async () => {
+//     let userurl = "https://jsonplaceholder.typicode.com/users"
+
+// let watch = await fetch(userurl);
+// let userjson = await watch.json();
+// console.log(userjson);
+
+// }
+
+// url()
+
+
+
+//////////////toq va juft////////////////////
+
+//
+
+// let num = prompt("son")
+// for(let i=0; i<=num; i++){
+//     if(i%2 == 0){
+//         console.log('juft', i);
+//     }
+//    else{ console.log('toq', i);}
+// }
+
+ 
+////////////////Sonlar summasi//////////////////////////
+
+// let sum = 0
+
+// for(let i=0; i<=5; i++){
+//     sum = sum +i
+// }
+// console.log(sum);
+
+
+
+////////////////// yil oy va hafta////////////////////
+
+// function yil(son) {
+//   return ` ${son*1} yil ${son*12} oy ${son*52} hafta`  
+// }
+// alert(yil(2));
+
+
+
+/////////Sonni ichidagi toq sonlar/////////////////// 
+
+// function oddcount(param) {
+//     return Math.round(param/2)
+// }
+// console.log(oddcount(13));
+
+
+//////////Manfiy raqam////////////////////////////
+
+// function manfiy(raqam) {
+//   if(raqam == 0){
+//     return 0
+//   }
+//   else{
+//     return -raqam 
+//   }
+// }
+// console.log(manfiy(8));
